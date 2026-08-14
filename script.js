cd05be5666e020bf0c35e5e06e7dfa486e5a70e4
Darbar Krishna <krishnadarbar10@gmail.com>
2026-08-14 12:03:10 +0530

    Add typing effect to hero

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ navLinks.forEach(function (link) {
     });
 });
 
+// Typing effect for the hero section
+
+const typingText = document.getElementById("typing-text");
+
+const text = "CSE AIML Student & Developer";
+
+let index = 0;
+
+function typeText() {
+
+    if (index < text.length) {
+        typingText.textContent += text.charAt(index);
+
+        index++;
+
+        setTimeout(typeText, 80);
+    }
+}
+
+typeText();
